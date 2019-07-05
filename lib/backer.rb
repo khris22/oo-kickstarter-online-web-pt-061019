e877a813ebe4388b2fa690984cb0af959c797834
Khris Punzalan <khrispunzalan@gmail.com>
2019-07-05 11:24:06 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -4,10 +4,12 @@ class Backer
   attr_accessor :name
   attr_reader :backed_projects
 
-def initialize(name)
-  @name = name
-  @backed_projects = []
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
 
-end
-
+  def back_project(Project)
+    @backed_projects << Project.new
+  end
 end
